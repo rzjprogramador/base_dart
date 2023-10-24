33407691db9ed0660f2ec158892820ac87db6b22
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-10-24 10:40:46 -0300

    feat: valores default em dart

--- a_referencia_de_informacao_Unitaria/default.valorZero.dart
+++ a_referencia_de_informacao_Unitaria/default.valorZero.dart
@@ -12,6 +12,6 @@ void main() {
 
 /*
 valores zero por default
-ao contrario de outras linguagens que por exemplo o valor default de string pe string vazia, de numero é zero,
-em dart para permitir que uma referencia possa nao receber valor tem que demarcar com ? possivel nulo o tipoQueRetornará e se nada for preenchido o valor zero de todos os tipos posssiveis nulos é nulo / null por isto apra usar tem que verificar se é nulo ou nao antes de fazer algo
+na maioria das linguagens o valor default é zerado o primeiro valor que se pode dar a um tipo, ex: de string é string vazia, de numero é zero, de logico é false. Mas tem inguagem que inicia com null todos tipos de referencia por exemplo dart é assim.
+Em dart para permitir que uma referencia possa nao receber valor tem que demarcar com ? possivel nulo o tipoQueRetornará e se nada for preenchido o valor zero de todos os tipos posssiveis nulos é nulo / null por isto apra usar tem que verificar se é nulo ou nao antes de fazer algo
 */
