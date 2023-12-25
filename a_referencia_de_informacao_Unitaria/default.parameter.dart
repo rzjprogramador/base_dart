a4fff8eb0e13beed461dd3d566ad8fccc7561e1c
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-12-24 21:25:06 -0300

    feat: relembrando dart

--- a_referencia_de_informacao_Unitaria/default.parameter.dart
+++ a_referencia_de_informacao_Unitaria/default.parameter.dart
@@ -1,7 +1,12 @@
-String meuTexto( {String texto = "valor do default param texto"}) {
+String meuTexto_Interno({String texto = "valor do default param texto"}) {
+  return texto;
+}
+
+String meuTexto_Vindo_do_Externo(String texto) {
   return texto;
 }
 
 void main() {
-  print(meuTexto());
+  print(meuTexto_Interno());
+  print(meuTexto_Vindo_do_Externo("O texto veio de fora por param"));
 }
--- a_referencia_de_informacao_Unitaria/tipos_primitivos.dart
+++ a_referencia_de_informacao_Unitaria/tipos_primitivos.dart
@@ -4,10 +4,14 @@ void main() {
   int numero_inteiro = 10;
   double numero_decimal = 11.10;
   bool logico = true;
+  dynamic any_posso_mudar_tipo_do_valor_na_sequencia = 'any_texto';
+  any_posso_mudar_tipo_do_valor_na_sequencia = 1000;
+  any_posso_mudar_tipo_do_valor_na_sequencia = true;
 
   print(caractere);
   print(texto);
   print(numero_inteiro);
   print(numero_decimal);
   print(logico);
+  print(any_posso_mudar_tipo_do_valor_na_sequencia);
 }
